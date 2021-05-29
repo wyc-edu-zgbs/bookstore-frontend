63a85af1ab5606ed4dbe76a1468b0f09a1da1351
wyc <wyc-edu-zgbs@users.noreply.github.com>
2021-05-29 11:53:13 +0800

    fix changes in search view caused by merge

--- src/components/ItemsView.vue
+++ src/components/ItemsView.vue
@@ -43,48 +43,54 @@ export default {
 			demo_products: {
 				"pages": 12,
 				"contents": [
-				{
-					"name": "寻找《局外人》",
-					"cover": "s33658199.jpg",
-					"author": "[美]爱丽丝·卡普兰 ",
-					"price": 98.0,
-					"id": "d99a716c-b952-11eb-8bfc-f1ff82be21dd"
-				},
-				{
-					"name": "不要和你妈争辩",
-					"cover": "s33610259.jpg",
-					"author": "[瑞典] 弗雷德里克·巴克曼 ",
-					"price": 39.8,
-					"id": "d99a716d-b952-11eb-8bfc-f1ff82be21dd"
-				},
-				{
-					"name": "鞋带",
-					"cover": "s33601424.jpg",
-					"author": "[意] 多梅尼科·斯塔尔诺内 ",
-					"price": 45,
-					"id": "d99a716e-b952-11eb-8bfc-f1ff82be21dd"
-				},
-				{
-					"name": "正常人",
-					"cover": "s33684681.jpg",
-					"author": "[爱尔兰] 萨莉·鲁尼 ",
-					"price": 49.8,
-					"id": "d99a716f-b952-11eb-8bfc-f1ff82be21dd"
-				},
-				{
-					"name": "光明共和国",
-					"cover": "s33625558.jpg",
-					"author": "[西]安德烈斯·巴尔瓦 ",
-					"price": 46,
-					"id": "d99a7170-b952-11eb-8bfc-f1ff82be21dd"
-				},
-				{
-					"name": "往复书简：初恋与不伦",
-					"cover": "s33668217.jpg",
-					"author": "[日] 坂元裕二 ",
-					"price": 42.0,
-					"id": "d99a7171-b952-11eb-8bfc-f1ff82be21dd"
-				}
+					{
+						"name": "寻找《局外人》",
+						"cover": "s33658199.jpg",
+						"author": "[美]爱丽丝·卡普兰 ",
+						"price": 98.0,
+						"id": "d99a716c-b952-11eb-8bfc-f1ff82be21dd",
+						"rating": 0.8
+					},
+					{
+						"name": "不要和你妈争辩",
+						"cover": "s33610259.jpg",
+						"author": "[瑞典] 弗雷德里克·巴克曼 ",
+						"price": 39.8,
+						"id": "d99a716d-b952-11eb-8bfc-f1ff82be21dd",
+						"rating": 0.8
+					},
+					{
+						"name": "鞋带",
+						"cover": "s33601424.jpg",
+						"author": "[意] 多梅尼科·斯塔尔诺内 ",
+						"price": 45,
+						"id": "d99a716e-b952-11eb-8bfc-f1ff82be21dd",
+						"rating": 0.8
+					},
+					{
+						"name": "正常人",
+						"cover": "s33684681.jpg",
+						"author": "[爱尔兰] 萨莉·鲁尼 ",
+						"price": 49.8,
+						"id": "d99a716f-b952-11eb-8bfc-f1ff82be21dd",
+						"rating": 0.8
+					},
+					{
+						"name": "光明共和国",
+						"cover": "s33625558.jpg",
+						"author": "[西]安德烈斯·巴尔瓦 ",
+						"price": 46,
+						"id": "d99a7170-b952-11eb-8bfc-f1ff82be21dd",
+						"rating": 0.8
+					},
+					{
+						"name": "往复书简：初恋与不伦",
+						"cover": "s33668217.jpg",
+						"author": "[日] 坂元裕二 ",
+						"price": 42.0,
+						"id": "d99a7171-b952-11eb-8bfc-f1ff82be21dd",
+						"rating": 0.8
+					}
 				]
 			}
 		}
@@ -140,6 +146,11 @@ export default {
 
 <style scoped>
 .el-pagination {
-	text-align: center
+	text-align: center;
+}
+
+.result {
+	padding: 3% 3%;
+	background: rgba(255, 255, 255, 0.5);
 }
 </style>
