1809da9565cbadf61a0f15026db1ad61bb792fb8
937547764 <937547764@qq.com>
2021-05-29 12:12:30 +0800

    merge

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -6,20 +6,12 @@
 </template>
 
 <script>
-<<<<<<< Updated upstream
-import ProductItem from '../components/ProductItem.vue'
-=======
 import ItemsView from '../components/ItemsView.vue'
->>>>>>> Stashed changes
 import SearchBox from '../components/SearchBox.vue'
 
 export default {
   components: {
-<<<<<<< Updated upstream
-    ProductItem,
-=======
     ItemsView,
->>>>>>> Stashed changes
     SearchBox
   }
 }
