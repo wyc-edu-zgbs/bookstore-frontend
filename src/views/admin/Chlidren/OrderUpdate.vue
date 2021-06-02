f9e58249bac2e2592be22ad76ddb074991482f2b
Quasimoodo <63183315+Quasimoodo@users.noreply.github.com>
2021-06-02 17:43:00 +0800

    订单页完成

--- src/views/admin/Chlidren/OrderUpdate.vue
+++ src/views/admin/Chlidren/OrderUpdate.vue
@@ -12,9 +12,57 @@
         <el-table :data="order" height=666px style="width: 100%" :default-sort = "{prop: 'date', order: 'descending'}" 
                 :header-cell-style="{background:'rgba(58, 130, 119,0.7)',color:'white'}"
                 :row-style="{height: '30px'}">
-            
+         <el-table-column type="expand">
+      <template slot-scope="scope">
+<!--        <el-form label-position="left" inline class="demo-table-expand">
+        </el-form>-->
+        <el-table :data='scope.row.items' style="width: 100%"
+                        :header-cell-style="{background:'rgba(58, 130, 119,0.7)',color:'white'}"
+                :row-style="{height: '20px'}">
+                <el-table-column prop="name" label="图书名称">
+                </el-table-column>
+                <el-table-column prop='price' label="价格">
+                </el-table-column>
+                <el-table-column prop="count" label="数量">
+                </el-table-column>
+                <el-table-column label='共计'>
+          <template slot-scope="scope">
+            {{scope.row.price * scope.row.count | formatPrice}}
+          </template>                    
+                </el-table-column>
+
+        </el-table>
+      </template>
+         </el-table-column>
+
+            <!--
             <el-table-column prop="id" label="订单id" @contextmenu.prevent="" width="300px"></el-table-column>
+            -->
             <el-table-column prop="user" label="用户id" ></el-table-column>
+            <el-table-column label='预览'>
+          <template slot-scope="scope">
+
+<el-row :gutter="20" style="margin-top:20px">
+  <el-col v-for="item in scope.row.items.slice(0,2)" :key=item.id :span="6">
+              <div  >
+                  <img :src="item.cover | media2url" width="40" height="40">
+              </div>
+
+              
+              </el-col>
+              <el-col v-if='scope.row.items.length>2' :span="6">
+                         <i  class="el-icon-more" style="margin-top:10px;margin-left:10px"></i>
+              </el-col>
+
+
+</el-row>
+
+              <!--
+            <el-image v-for="item in scope.row.items.slice(0,3)" :key=item.cover :src="item.cover" width="40" height="40">
+            </el-image>
+            -->
+          </template>         
+            </el-table-column>
             <el-table-column prop="time" label="下单时间" ></el-table-column>   
             <el-table-column prop="state" label="订单状态" >
              <template slot-scope="scope">
@@ -25,8 +73,15 @@
                 
                 </el-table-column>  
 
-            <el-table-column fixed="right" width="50">
+                          <el-table-column  label="订单金额" >
+                <template slot-scope="scope">
 
+          {{scope.row.items.map(x=>x.count*x.price).reduce((a,b)=>a+b, 0) | formatPrice}}
+                </template>
+            </el-table-column>
+
+            <el-table-column fixed="right" width="50">
+  
             <template slot-scope="scope">
                 <el-tooltip class="item" effect="dark" content="发货" placement="bottom-end">
                   <el-button @click.native="pushorder(scope.row.id)" type="text" style="color: #999" size="medium">
@@ -95,8 +150,51 @@ export default {
               "user":'00135',
               "time":"2021-05-08 23:55",
               "state":"1",
-
-}
+              items: [
+        {
+          "count": 7,
+          "id": "ad727512-bd80-11eb-a8b8-c1e635d27859",
+          "name": "寻找《局外人》",
+          "cover": "s33658199.jpg",
+          "price": 98.0
+        },
+        {
+          "count": 9,
+          "id": "ad727513-bd80-11eb-a8b8-c1e635d27859",
+          "name": "不要和你妈争辩",
+          "cover": "s33610259.jpg",
+          "price": 39.8
+        },
+        {
+          "count": 5,
+          "id": "ad727514-bd80-11eb-a8b8-c1e635d27859",
+          "name": "鞋带",
+          "cover": "s33601424.jpg",
+          "price": 45
+        },
+        {
+          "count": 6,
+          "id": "ad727515-bd80-11eb-a8b8-c1e635d27859",
+          "name": "正常人",
+          "cover": "s33684681.jpg",
+          "price": 49.8
+        },
+        {
+          "count": 9,
+          "id": "ad727516-bd80-11eb-a8b8-c1e635d27859",
+          "name": "光明共和国",
+          "cover": "s33625558.jpg",
+          "price": 46
+        },
+        {
+          "count": 2,
+          "id": "ad727517-bd80-11eb-a8b8-c1e635d27859",
+          "name": "往复书简：初恋与不伦",
+          "cover": "s33668217.jpg",
+          "price": 42.0
+        }
+      ]
+          }
 
       ],
       buyNum: 1
@@ -145,3 +243,14 @@ export default {
 
 }
 </script>
+
+<style >
+
+  .el-row {
+    margin-bottom: 20px;
+
+  }
+  .el-col {
+    border-radius: 4px;
+  }
+</style>
