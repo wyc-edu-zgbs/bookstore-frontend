c8ef8953aa8e352b2c891d2e3b79364736233462
wyc <wyc-edu-zgbs@users.noreply.github.com>
2021-06-15 21:22:59 +0800

    add urls to HomeCarousel

--- src/views/home/HomeCarousel.vue
+++ src/views/home/HomeCarousel.vue
@@ -4,16 +4,18 @@
       trigger="click"
       height="40vh"
       class="car"
-      interval="2000"
+      :interval="2000"
     >
       <el-carousel-item
         v-for="item in images"
         :key="item.id"
       >
-        <img
-          :src="item.idView"
-          class="image"
-        >
+        <router-link :to="item.url ? item.url : '/'">
+          <img
+            :src="item.idView"
+            class="image"
+          >
+        </router-link>
       </el-carousel-item>
     </el-carousel>
     <el-divider id="divi"></el-divider>
@@ -29,11 +31,31 @@ export default {
   data() {
     return {
       images: [
-        { id: 0, idView: require('../../assets/homecarImage/0.jpg') },
-        { id: 1, idView: require('../../assets/homecarImage/1.jpg') },
-        { id: 2, idView: require('../../assets/homecarImage/2.jpg') },
-        { id: 3, idView: require('../../assets/homecarImage/3.jpg') },
-        { id: 4, idView: require('../../assets/homecarImage/4.jpg') }
+        {
+          id: 0,
+          idView: require('../../assets/homecarImage/0.jpg'),
+          url: "/book/9787513344456"
+        },
+        {
+          id: 1,
+          idView: require('../../assets/homecarImage/1.jpg'),
+          url: "/search?q=%E5%88%98%E6%85%88%E6%AC%A3"
+        },
+        {
+          id: 2,
+          idView: require('../../assets/homecarImage/2.jpg'),
+          url: "/book/9787555911425"
+        },
+        {
+          id: 3,
+          idView: require('../../assets/homecarImage/3.jpg'),
+          url: "/book/9787515521763"
+        },
+        {
+          id: 4,
+          idView: require('../../assets/homecarImage/4.jpg'),
+          url: "/book/9787518961481"
+        }
       ],
     }
   },
